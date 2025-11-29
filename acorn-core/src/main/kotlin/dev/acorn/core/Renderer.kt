767b969bc51fa3a2d0ab04979eba00849cec8a59
aznos <aznosreal@gmail.com>
2025-11-28 19:00:17 -0500

    extend renderer

--- acorn-core/src/main/kotlin/dev/acorn/core/Renderer.kt
+++ acorn-core/src/main/kotlin/dev/acorn/core/Renderer.kt
@@ -2,4 +2,7 @@ package dev.acorn.core
 
 interface Renderer {
     fun clear(r: Float, g: Float, b: Float, a: Float)
+
+    fun drawRect(transform: Transform, color: Color)
+    fun drawCircle(transform: Transform, color: Color, segments: Int = 32)
 }
