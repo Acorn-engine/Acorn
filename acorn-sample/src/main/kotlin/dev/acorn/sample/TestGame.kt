8f64875b3c58ba4dfb22b969c59d2ed20d8db4d7
aznos <aznosreal@gmail.com>
2025-11-28 21:15:16 -0500

    test game setup putting everything together

--- acorn-sample/src/main/kotlin/dev/acorn/sample/TestGame.kt
+++ acorn-sample/src/main/kotlin/dev/acorn/sample/TestGame.kt
@@ -1,33 +1,54 @@
 package dev.acorn.sample
 
 import dev.acorn.core.Acorn
-import dev.acorn.core.gameobject.GameContext
+import dev.acorn.core.Color
 import dev.acorn.core.Renderer
-import dev.acorn.core.gameobject.WindowConfig
+import dev.acorn.core.Vec2
+import dev.acorn.core.gameobject.*
 import kotlin.math.sin
 
 class TestGame : Acorn {
-    private var hueTime = 0f
+    private val scene = Scene()
+    private var time = 0f
+
+    private lateinit var rotatingSquare: GameObject
+    private lateinit var movingCircle: GameObject
 
     override fun configureWindow(config: WindowConfig) {
-        config.width = 1920
-        config.height = 1080
+        config.width = 1280
+        config.height = 720
         config.title = "Acorn test game"
     }
 
     override fun setup(context: GameContext) {
-        println("TestGame started")
+        rotatingSquare = scene.createGameObject(Transform(
+            position = Vec2(640f, 360f),
+            scale = Vec2(100f, 100f)
+        )).addComponent(SpriteShapeRenderer(
+            shape = ShapeType.RECT,
+            color = Color.fromRGB(0xFFAA00))
+        )
+
+        movingCircle = scene.createGameObject(Transform(
+            position = Vec2(200f, 200f),
+            scale = Vec2(80f, 80f)
+        )).addComponent(SpriteShapeRenderer(
+            shape = ShapeType.CIRCLE,
+            color = Color.fromRGB(0x00CCFF))
+        )
     }
 
     override fun update(dtSeconds: Float) {
-        hueTime += dtSeconds
+        time += dtSeconds
+
+        rotatingSquare.transform.rotationDeg = time * 60f
+        movingCircle.transform.position.x = 640f + sin(time) * 200f
+
+        scene.update(dtSeconds)
     }
 
     override fun render(renderer: Renderer) {
-        val r = (0.5f + 0.5f * sin(hueTime)).coerceIn(0f, 1f)
-        val g = (0.5f + 0.5f * sin(hueTime + 2f)).coerceIn(0f, 1f)
-        val b = (0.5f + 0.5f * sin(hueTime + 4f)).coerceIn(0f, 1f)
-
-        renderer.clear(r, g, b, 1f)
+        renderer.clear(0.1f, 0.1f, 0.1f, 1f)
+        scene.render(renderer)
     }
 }
