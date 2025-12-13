494fa5ebbc7d544aef09a3cf0b68743ad4c25796
aznos <aznosreal@gmail.com>
2025-12-13 16:45:13 -0500

    Add some javadocs to the acorn core, mainly in scene

--- acorn-core/src/main/kotlin/dev/acorn/core/scene/GameObject.kt
+++ acorn-core/src/main/kotlin/dev/acorn/core/scene/GameObject.kt
@@ -2,9 +2,20 @@ package dev.acorn.core.scene
 
 import dev.acorn.core.render.Renderer
 
+/**
+ * The core of acorn, a [dev.acorn.core.scene.GameObject] is something in the game. A [dev.acorn.core.scene.GameObject] contains [Component]'s
+ *
+ * @param transform The transform of the [dev.acorn.core.scene.GameObject]
+ */
 class GameObject(val transform: Transform = Transform()) {
     private val components = mutableListOf<Component>()
 
+    /**
+     * Adds a [Component] onto a [dev.acorn.core.scene.GameObject]
+     *
+     * @param component The [Component] you're adding
+     * @return The new [dev.acorn.core.scene.GameObject] with the [Component] applied
+     */
     fun addComponent(component: Component): GameObject {
         component.gameObject = this
         components += component
@@ -12,21 +23,40 @@ class GameObject(val transform: Transform = Transform()) {
         return this
     }
 
+    /**
+     * Removes a [Component] from a [dev.acorn.core.scene.GameObject]
+     *
+     * @param component The [Component] you're removing
+     * @return The new [dev.acorn.core.scene.GameObject] with the [Component] removed
+     */
     fun removeComponent(component: Component) {
         if(components.remove(component)) {
             component.onRemoved()
         }
     }
 
+    /**
+     * Finds a [Component] from a [dev.acorn.core.scene.GameObject]
+     */
     fun <T : Component> getComponent(type: Class<T>): T? {
         @Suppress("UNCHECKED_CAST")
         return components.firstOrNull { type.isInstance(it) } as? T
     }
 
+    /**
+     * Updates all the [Component]'s on the [dev.acorn.core.scene.GameObject]
+     *
+     * @param dt The delta seconds
+     */
     fun update(dt: Float) {
         components.forEach { it.update(dt) }
     }
 
+    /**
+     * Renders all the [Component]'s on the [dev.acorn.core.scene.GameObject] onto the screen
+     *
+     * @param renderer The main renderer
+     */
     fun render(renderer: Renderer) {
         components.forEach { it.render(renderer) }
     }
--- acorn-core/src/main/kotlin/dev/acorn/core/scene/Transform.kt
+++ acorn-core/src/main/kotlin/dev/acorn/core/scene/Transform.kt
@@ -2,6 +2,13 @@ package dev.acorn.core.scene
 
 import dev.acorn.core.math.Vec2
 
+/**
+ * Transform [Component], this stores the position, rotation, and scale of a [GameObject]
+ *
+ * @param position The position (in pixels) of a [GameObject]
+ * @param rotationDeg The rotation in degrees (0-360) of a [GameObject]
+ * @param scale The scale of a [GameObject]
+ */
 data class Transform(
     var position: Vec2 = Vec2(0f, 0f),
     var rotationDeg: Float = 0f,
