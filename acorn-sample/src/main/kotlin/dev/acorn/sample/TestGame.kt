4c75ad9520b9abd63676b5d0f97f10e515ca62f2
aznos <aznosreal@gmail.com>
2026-01-07 17:11:24 -0500

    Linked everything up

--- acorn-sample/src/main/kotlin/dev/acorn/sample/TestGame.kt
+++ acorn-sample/src/main/kotlin/dev/acorn/sample/TestGame.kt
@@ -1,23 +1,40 @@
 package dev.acorn.sample
 
 import dev.acorn.core.app.AcornGame
+import dev.acorn.core.components.SpriteShapeRenderer
 import dev.acorn.core.input.Keys
 import dev.acorn.core.math.Vec2
 import dev.acorn.core.math.normalized
 import dev.acorn.core.math.plusAssign
 import dev.acorn.core.math.times
+import dev.acorn.core.physics.BodyType
+import dev.acorn.core.physics.collider.BoxCollider
+import dev.acorn.core.physics.event.CollisionEnter
 import dev.acorn.core.scene.GameObject
+import dev.acorn.core.scene.Transform
 import dev.acorn.core.scene.spriteObject
 
 class TestGame : AcornGame() {
     private var time = 0f
     private lateinit var player: GameObject
-
     private var moveSpeed = 250f
 
     override fun onStart() {
         val playerSprite = sprite("67.jpeg")
-        player = scene.spriteObject(playerSprite, center(), Vec2(128f, 128f))
+        player = scene.spriteObject(playerSprite, center(), Vec2(128f, 128f)).apply {
+            addComponent(BoxCollider())
+        }
+
+        spawn(Transform(position = Vec2(center().x + 200f, center().y), scale = Vec2(150f, 300f))).apply {
+            addComponent(SpriteShapeRenderer())
+            addComponent(BoxCollider().apply { bodyType = BodyType.Static })
+        }
+
+        scene.collisions().events.subscribe { e ->
+            if(e is CollisionEnter && e.selfObject == player) {
+                println("Player hit ${e.otherObject.id}")
+            }
+        }
     }
 
     override fun onUpdate(dt: Float) {
