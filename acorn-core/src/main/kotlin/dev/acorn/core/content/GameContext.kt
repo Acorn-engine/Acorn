9fc5331a9f4281738d3058b0496bf7c457e9c12f
aznos <aznosreal@gmail.com>
2026-01-11 18:21:02 -0500

    Core API for debug drawing

--- acorn-core/src/main/kotlin/dev/acorn/core/content/GameContext.kt
+++ acorn-core/src/main/kotlin/dev/acorn/core/content/GameContext.kt
@@ -1,6 +1,7 @@
 package dev.acorn.core.content
 
 import dev.acorn.core.assets.TextureService
+import dev.acorn.core.debug.DebugDraw
 import dev.acorn.core.input.Input
 import dev.acorn.core.time.Time
 import dev.acorn.core.window.Window
@@ -12,10 +13,12 @@ import dev.acorn.core.window.Window
  * @property textures All the textures in the game
  * @property input Any input from the keyboard or mouse
  * @property time The time service
+ * @property debug Debug service
  */
 interface GameContext {
     val window: Window
     val textures: TextureService
     val input: Input
     val time: Time
+    val debug: DebugDraw
 }
